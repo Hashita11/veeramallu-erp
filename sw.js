f89fb1eaa0f2e77057cad248309d84f989bde80e
Hashita11 <hbmlcapital@gmail.com>
2026-07-11 17:48:11 +0530

    Update service worker cache version and events
    Updated cache version and added activation event handling.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vga-school-app-v1";
+const CACHE_NAME = "vga-school-app-v2";
 
 const urlsToCache = [
   "/",
@@ -20,13 +20,34 @@ const urlsToCache = [
   "/launchericon-512x512.png",
 ];
 
+// Install
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
